ed6ce2d116631d771a1a468382f7ae821a58ed1b
ProDev036 <usamashahid014@gmail.com>
2024-09-09 19:09:55 +0500

    Included the raw int replacement script

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ if(total_months>=38){
         months_array[i]=29;
     }
 }
-
-console.log(months_array)
+//Uncomment the line elow to get view the months array.
+//console.log(months_array)
 
 //Defining Empty array and utilizing nested loop to get nested array of dutiees, later replasing nnummbers,
 
@@ -37,7 +37,34 @@ for(let i=0;i<=total_months-1;i++){
             if(reference1==26){reference1=1;}
         }
     }
-
 }
 
 console.log(duty1);
+
+//Converting the numbers to strings.
+
+//1,2,3,14,15 for N
+//4,5,16,17,18 for E
+//7,8,9,10,11 for M
+//6,12,13,19,20,21,22,23,24,25 for R
+
+//using array.map feature
+
+
+let final_dut1=duty1.map(item =>{ 
+    if(item===1 || item===2 || item===3 || item===14 || item===15){
+        return 'N';
+    }
+    else if(item===4 || item===5 || item===16 || item===17 || item===18){
+        return 'E';
+    }
+    else if (item===7 || item===8 || item===9 || item===10 || item===11){
+        return 'M'
+    }
+    else if (item===6 || item===12 || item===13 || item===19 || item===20 || item===21 || item===22 || item===23 || item==24 || item===25){
+        return 'R'
+    }
+    return item;
+}
+
+)
